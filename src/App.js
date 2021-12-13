96733f2ec93db10e9af596e4799c797abc43c569
Branimir Matijacic <bmatijacic@nsk.hr>
2021-12-13 10:51:18 +0100

    dodan kod u app.js za prikaz komponenata

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import FuncKomp from './components/FuncKomp';
+import KlasKomp from './components/KlasKomp';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <FuncKomp />
+        <KlasKomp />
       </header>
     </div>
   );
